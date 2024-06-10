8e79888e11683c75d5abdc5e3fc71cd2e9a1fd24
TOETOE55 <584605539@qq.com>
2024-06-10 11:46:10 +0800

    feat: qsort bench

--- benches/qsort/main.rs
+++ benches/qsort/main.rs
@@ -1,36 +1,44 @@
 use std::iter::repeat_with;
 
-use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use criterion::{criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion};
 use rand::{thread_rng, Rng};
 use simd_demo::qsort::{avx, normal};
 
 pub fn qsort_bench(c: &mut Criterion) {
     let mut rand = thread_rng();
-    let xs: Vec<i32> = repeat_with(|| rand.gen::<i32>()).take(1_000_000).collect();
 
-    c.bench_function("simd qsort", |b| {
-        b.iter_batched(
-            || xs.clone(),
-            |mut xs| avx::qsort(&mut xs),
-            BatchSize::SmallInput,
-        );
-    });
+    let mut group = c.benchmark_group("qsort");
 
-    c.bench_function("normal qsort", |b| {
-        b.iter_batched(
-            || xs.clone(),
-            |mut xs| normal::qsort(&mut xs),
-            BatchSize::SmallInput,
-        );
-    });
+    for count in [10, 100, 1000, 10_000, 100_000, 1_000_000] {
+        let xs: Vec<i32> = repeat_with(|| rand.gen::<i32>()).take(count).collect();
 
-    c.bench_function("std qsort", |b| {
-        b.iter_batched(
-            || xs.clone(),
-            |mut xs| xs.sort_unstable(),
-            BatchSize::SmallInput,
-        );
-    });
+        group.bench_with_input(BenchmarkId::new("simd", count), &xs, |b, xs| {
+            b.iter_batched(
+                || xs.clone(),
+                |mut xs| avx::qsort(&mut xs),
+                BatchSize::SmallInput,
+            );
+        });
+
+    
+        group.bench_with_input(BenchmarkId::new("normal", count), &xs, |b, xs| {
+            b.iter_batched(
+                || xs.clone(),
+                |mut xs| normal::qsort(&mut xs),
+                BatchSize::SmallInput,
+            );
+        });
+
+        group.bench_with_input(BenchmarkId::new("std", count), &xs, |b, xs| {
+            b.iter_batched(
+                || xs.clone(),
+                |mut xs| xs.sort_unstable(),
+                BatchSize::SmallInput,
+            );
+        });
+    }
+
+    group.finish();
 }
 criterion_group!(benches, qsort_bench);
 criterion_main!(benches);
